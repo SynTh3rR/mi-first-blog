448d63e4c6eb4fddbe50429788b5681225cb4528
gabriel <mystt1cs623@gmail.com>
2024-07-24 16:43:49 -0400

    se añadio unas vistas con urls y los dellates del blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('hola', views.ola),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
